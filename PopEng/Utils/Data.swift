2200c24b9e4b712de6b89fc3d7d6f798d8acd67f
Adam Khalifa <70577640+geekMe1982@users.noreply.github.com>
2025-01-04 08:52:20 +0300

    update data

--- PopEng/Utils/Data.swift
+++ PopEng/Utils/Data.swift
@@ -6,11 +6,22 @@
 //
 
 import Foundation
+import FlagsKit
 
 struct Data {
-    static let questions: [Question] = [
-        Question(query: "What would you like to learn?", answerImg: ["russia", "saudi-arabia"], answerText: [], answerSubText: ["Russian", "Arabic"]),
-        Question(query: "What's your current level?", answerImg: ["fly", "hen", "diplodocus" ], answerText: ["I'm new to this language", "Iknow some words/phrases", "I'm intermediate or heigher"], answerSubText: [])
-        
+    static let questions: [Question] =
+    [
+        Question(query: "What's your current level?",
+                 answerImg: ["fly", "hen", "diplodocus"],
+                 answerText: ["I'm new to this language", "I know some words/phrases", "I'm intermediate or heigher"],
+                 answerSubText: []),
+        Question(query: "How much of the language do you know?",
+                 answerImg: ["graph1", "graph2", "graph3", "graph4"],
+                 answerText: ["I'm new to this language", "I know a few words/phrases", "I can engage in a simple conversation", "I know much"],
+                 answerSubText: []),
+        Question(query: "What's you daily learning goal?",
+                 answerImg: [],
+                 answerText: ["10 min/day", "20 min/day", "30 min/day"],
+                 answerSubText: ["Casual", "Regular", "Serious", "Intense"])
     ]
 }
