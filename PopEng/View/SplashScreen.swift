0a66d08d915f27161ee55d8d28173d5c86c87675
Adam Khalifa <70577640+geekMe1982@users.noreply.github.com>
2025-01-04 08:56:41 +0300

    create splashScreen view

--- PopEng/View/SplashScreen.swift
+++ PopEng/View/SplashScreen.swift
@@ -7,9 +7,36 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct SplashScreen: View {
+    @State private var show = false
+    @State private var showHomeView = false
+    
     var body: some View {
-        Text("SplashScreen")
+        GeometryReader { geo in
+            VStack(spacing: 0) {
+                ZStack {
+                    Image(.popBalloon)
+                        .resizable().scaledToFill()
+                        .frame(width: 300, height: 300)
+                        .offset(y: showHomeView ? -geo.size.height : 0)
+                }
+                .frame(width: geo.size.width, height: geo.size.height)
+                .background(Image(.splashBackground).resizable().scaledToFill()
+                )
+                GetStartedView()
+                    .frame(width: geo.size.width, height: geo.size.height)
+                    .offset(y: showHomeView ? -geo.size.height : 0)
+            }
+            .onAppear() {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2){
+                    withAnimation(.linear(duration: 1)){
+                        showHomeView = true
+                    }
+                }
+            }
+        }.ignoresSafeArea()
     }
 }
 
