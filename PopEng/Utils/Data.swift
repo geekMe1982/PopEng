f024e38693e10c462ba68348f5fa2ea2cb0c90e6
Adam Khalifa <70577640+geekMe1982@users.noreply.github.com>
2025-01-07 17:46:19 +0300

    rename model

--- PopEng/Utils/Data.swift
+++ PopEng/Utils/Data.swift
@@ -9,19 +9,19 @@ import Foundation
 import FlagsKit
 
 struct Data {
-    static let questions: [Question] =
+    static let questions: [QuestionModel] =
     [
-        Question(query: "What's your current level?",
+        QuestionModel(query: "What's your current level?",
                  answerImg: [Images.LLevel1, Images.LLevel2, Images.LLevel3],
                  answerText: ["I'm new to this language", "I know some words/phrases", "I'm intermediate or heigher"],
                  answerSubText: []),
-        Question(query: "How much of the language do you know?",
-                 answerImg: [Images.knowlegde1, Images.knowlegde2, Images.knowlegde3, Images.knowlegde4],
+        QuestionModel(query: "How much of the language do you know?",
+                 answerImg: [Images.knowlegde1, Images.knowlegde2, Images.knowlegde3],
                  answerText: ["I studied English in school", "I took a course/hired a tutor", "I can engage in a simple conversation", "I know much"],
                  answerSubText: []),
-        Question(query: "What's you daily learning goal?",
-                 answerImg: [],
+        QuestionModel(query: "What's you daily learning goal?",
+                 answerImg: [Images.knowlegde1, Images.knowlegde2, Images.knowlegde3],
                  answerText: ["10 min/day", "20 min/day", "30 min/day"],
-                 answerSubText: ["Casual", "Regular", "Serious", "Intense"])
+                 answerSubText: [])
     ]
 }
