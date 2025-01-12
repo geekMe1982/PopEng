2a338aa1048018eab100776b0dd75d1a23e4998f
Adam Khalifa <70577640+geekMe1982@users.noreply.github.com>
2025-01-12 10:43:02 +0300

    out of bound fixed

--- PopEng/View/SelectionCardView.swift
+++ PopEng/View/SelectionCardView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct SelectionCardView: View {
     
-    @Binding var question: Question
+    @Binding var question: QuestionModel
     @Binding var selectedEntry: [Bool]
+    
     
     var queryIndex: Int
     var selectedIndex: Int
@@ -22,39 +23,41 @@ struct SelectionCardView: View {
             }
             selectedEntry[selectedIndex] = true
         }) {
-            ZStack(alignment: .leading) {
-                HStack(spacing: 20) {
-                    if(question.answerImg.count>0) {
-                        Image(question.answerImg[selectedIndex])
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 50, height: 50)
-                            .padding(.leading, 40)
-                    } else {
-                        Spacer()
-                            .frame(width: 20)
-                    }
-                    Text(question.answerText[selectedIndex])
-                        .font(.system(size: 18))
-                        .foregroundColor(.black)
-                        .bold()
-                        .multilineTextAlignment(.leading)
-                    Spacer()
-                    if(question.answerSubText.count>0) {
-                        Text(question.answerSubText[selectedIndex])
-                            .font(.system(size: 15))
+            if selectedIndex < selectedEntry.count {
+                ZStack(alignment: .leading) {
+                    HStack(spacing: 20) {
+                        if(question.answerImg.count>0) {
+                            Image(question.answerImg[selectedIndex])
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                                .padding(.leading, 40)
+                        } else {
+                            Spacer()
+                                .frame(width: 20)
+                        }
+                        Text(question.answerText[selectedIndex])
+                            .font(.system(size: 18))
                             .foregroundColor(.black)
                             .bold()
-                            .multilineTextAlignment(.trailing)
-                            .padding(.trailing, 50)
-                        
+                            .multilineTextAlignment(.leading)
+                        Spacer()
+                        if(question.answerSubText.count>0) {
+                            Text(question.answerSubText[selectedIndex])
+                                .font(.system(size: 15))
+                                .foregroundColor(.black)
+                                .bold()
+                                .multilineTextAlignment(.trailing)
+                                .padding(.trailing, 50)
+                            
+                        }
                     }
+                    RoundedRectangle (cornerRadius: 20)
+                        .stroke(selectedEntry[selectedIndex] ? .purple : .gray, lineWidth: 2)
+                        .frame(width: 350, height: 80)
+                        .padding()
+                        .disabled(!selectedEntry[selectedIndex])
                 }
-                RoundedRectangle (cornerRadius: 20)
-                    .stroke(selectedEntry[selectedIndex] ? .purple : .gray, lineWidth: 2)
-                    .frame(width: 350, height: 80)
-                    .padding()
-                    .disabled(!selectedEntry[selectedIndex])
             }
         }
     }
