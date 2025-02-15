d1742bdc1aea398931c13a8291c0cdd804531816
Adam Khalifa <70577640+geekMe1982@users.noreply.github.com>
2025-02-15 10:34:43 +0300

    .

---
+++ PopEng/Sources/Models/User.swift
@@ -0,0 +1,28 @@
+//
+//  User.swift
+//  PopEng
+//
+//  Created by Adam Khalifa on 11.02.2025.
+//
+
+import Foundation
+
+struct User: Identifiable, Codable {
+    let id: String
+    let fullName: String
+    let email: String
+    
+    var initials: String {
+        let formatter = PersonNameComponentsFormatter()
+        
+        if let components = formatter.personNameComponents(from: fullName) {
+            formatter.style = .abbreviated
+            return formatter.string(from: components)
+        }
+        return ""
+    }
+}
+
+extension User {
+    static var MOCK_USER = User(id: NSUUID().uuidString, fullName: "Adam Khalifa", email: "test@gmail.com")
+}
