97cf5cd42d97c926a77bb6f7c26a87c6fe79164e
Adam Khalifa <70577640+geekMe1982@users.noreply.github.com>
2025-02-15 10:34:20 +0300

    new component

---
+++ PopEng/Sources/Modules/Components/InputView.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftUIView.swift
+//  PopEng
+//
+//  Created by Adam Khalifa on 09.02.2025.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    @Binding var text: String
+    let title: String
+    let placeHolder: String
+    var isSecureField = false
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text(title)
+                .foregroundColor(Color(.darkGray))
+                .fontWeight(.semibold)
+                .font(.footnote)
+            if isSecureField {
+                SecureField(placeHolder, text: $text)
+                    .font(.system(size: 14))
+            }else{
+                TextField(placeHolder, text: $text)
+                    .font(.system(size: 14))
+            }
+            Divider()
+        }
+    }
+}
+
+#Preview {
+    InputView(text: .constant(""), title: "Email", placeHolder: "name@example.com")
+}
